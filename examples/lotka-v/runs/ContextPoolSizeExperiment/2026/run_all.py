405e1b965c07b56ed6e92e2d273083c09855db73
gb21553 <rd.nzoyemngueguin@bristol.ac.uk>
2024-07-23 20:18:48 +0100

    Fixed runall script for sequential runs

--- examples/lotka-v/runs/ContextPoolSizeExperiment/2026/run_all.py
+++ examples/lotka-v/runs/ContextPoolSizeExperiment/2026/run_all.py
@@ -4,4 +4,4 @@ files = [f"{i}.py" for i in [0, 1, 2, 4, 6, 8]]
 
 ## Run the files in succession. One must finish before the other starts
 for file in files:
-    os.system(f"nohup python {file} > nohup.log &")
+    os.system(f"python {file} > nohup.log")
