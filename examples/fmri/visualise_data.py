e9c4f718d236a3a01150c7c096a2b473abcea80c
Waldur Waldur <gb21553@bristol.ac.uk>
2025-01-01 20:50:21 +0000

    Amazing clustering, but the data doesn't look enough

---
+++ examples/fmri/visualise_data.py
@@ -0,0 +1,25 @@
+#%%
+# from selfmod import *
+import scipy.io as sio
+
+## Load the two matlab matrices in the data folder: 1. NBKO_PLRNN_dataset_RestReference.mat and 2. NBKO_PLRNN_dataset.mat
+
+# Load the data using scipy
+# data_ref = sio.loadmat('data/NBKO_PLRNN_dataset_RestReference.mat')
+data = sio.loadmat('data/NBKO_PLRNN_dataset.mat')
+
+# print(data.keys())
+# print(data['Data'].shape)
+
+X = data['Data'] ## shape (1,26)
+print(type(X[0,0]))
+# print(X)
+
+
+
+# ## Visualise the data
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# plt.figure(figsize=(10, 5))
+# plt.plot(X[0, :])
