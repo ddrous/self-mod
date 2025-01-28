f821820f05f94e73b6266c056c25175db0515e39
gb21553 <rd.nzoyemngueguin@bristol.ac.uk>
2025-01-28 10:42:07 +0000

    Losses plots for the 2 ODE problem

--- examples/ode-bench/plots/mixer_barplots.py
+++ examples/ode-bench/plots/mixer_barplots.py
@@ -79,4 +79,64 @@ ax.spines['right'].set_linewidth(2)
 ## Save as a PDF
 plt.savefig('mixer_barplots.pdf', bbox_inches='tight', dpi=300)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # %%
+
+## Increase the linewidth to 3
+plt.rcParams['lines.linewidth'] = 4
+
+## Plot the losses
+folders_1_expert = ["_02_NCFRuns/250120-210843-New1Expert/", "_03_CoDARuns/250119-232909-1Expert/", "_04_GEPSRuns/250120-193035-1Expert/"]
+folders_2_experts_gd = ["_02_NCFRuns/250120-002029-NewT2-GradientDescnet/", "_03_CoDARuns/250119-231018-2Experts-T0-GD/", "_04_GEPSRuns/250120-191238-2Experts-GD/"]
+folders_2_experts = ["_02_NCFRuns/250120-012332-NewT2/", "_03_CoDARuns/250119-224921-2Experts-T0/", "_04_GEPSRuns/250120-152120-2Experts-T0/"]
+
+## Make three colors for the three groups
+colors = ['b', 'r', 'g']
+markers = ['-', '-.', ':']
+
+## Load the train_history.npz files and plot the losses
+fig, ax = plt.subplots(1, 1, figsize=(12, 5))
+loss_key = 'losses_ctx'
+for i, folder in enumerate(folders_1_expert):
+    # history = np.load("../runs/"+folder + 'train_histories.npz')
+    # ax.plot(history[loss_key], label='Task-Specific ' + ['NCF', 'CoDA', 'GEPS'][i])
+    history = np.load("../runs/"+folder + 'val_losses.npy')
+    print(history.shape)
+    ax.plot(history[:, 0], history[:, 1], f"{colors[0]}{markers[i]}", label='Task-Specific ' + ['NCF', 'CoDA', 'GEPS'][i])
+
+## Do the same thing for the 2 experts GD
+for i, folder in enumerate(folders_2_experts_gd):
+    history = np.load("../runs/"+folder + 'val_losses.npy')
+    ax.plot(history[:, 0], history[:, 1], f"{colors[1]}{markers[i]}", label='Naive MoE ' + ['NCF', 'CoDA', 'GEPS'][i])
+
+## Do the same thing for the 2 experts
+for i, folder in enumerate(folders_2_experts):
+    history = np.load("../runs/"+folder + 'val_losses.npy')
+    ax.plot(history[:, 0], history[:, 1], f"{colors[2]}{markers[i]}", label='MixER ' + ['NCF', 'CoDA', 'GEPS'][i])
+
+ax.set_xlabel('Train Step', fontsize=22)
+ax.set_ylabel('MSE', fontsize=20)
+ax.set_yscale('log')
+
+## Increase size of ticks
+ax.tick_params(axis='both', which='major', labelsize=18)
+
+# ax.legend(loc='upper center', bbox_to_anchor=(0.5, 0.97), shadow=True, ncol=3, fontsize=14)
+ax.legend(loc='upper right', shadow=True, ncol=3, fontsize=12)
+
+## Save as a PDF
+plt.savefig('mixer_losses.pdf', bbox_inches='tight', dpi=300)
